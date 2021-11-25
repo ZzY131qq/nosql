36a5fc546266c7c88ffa844b919a51e69a3da023
shenyanjing <2647484467@qq.com>
2021-11-25 11:48:02 +0800

    搜索匹配加分词改进

--- 大作业/王鹏志-小说搜索引擎/noval/flask_book.py
+++ 大作业/王鹏志-小说搜索引擎/noval/flask_book.py
@@ -1,13 +1,21 @@
+from re import I
 from flask import Flask, redirect, url_for, request,render_template
 import pymongo
+import re
+import jieba
 
-
+#搜索匹配字符串
+def is_in(sub_str,full_str):
+    if re.findall(sub_str, full_str):
+        return full_str
+    else:
+        return False
 
 app = Flask(__name__)
 
 #连接mongo数据库
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["myfirstDB"]
+mydb = myclient["book_data"]
 mycol = mydb["bookdata"]
 
 @app.route('/')
@@ -48,18 +56,43 @@ def book_url(bookurl):
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
+      book_names = []
+      jieguo_yesandno = 0
       #接受前端发来的小说名字
       book_name = request.form['search']
-      #获取对应url值
-      myquery = {'book_name':{ "$regex": "^{}".format(book_name)}}
-      mydoc = mycol.find(myquery)
-      url = []
-      for x in mydoc:
-         url.append(x['book_url'])
-      if url:
-         return redirect(url_for('book_url',bookurl = url))
-      else:
+      #结巴分词处理
+      ss = jieba.cut_for_search(book_name) 
+      ss = (" ".join(ss))
+      ss = ss.split(" ")
+      #匹配书名
+      for x in mycol.find({},{ "_id": 0, "book_name": 1}):
+         for name in ss:
+            jieguo = is_in(name,x['book_name'])
+            if jieguo == False:
+               pass
+            else:
+               book_names.append(jieguo)
+               jieguo_yesandno += 1
+      if jieguo_yesandno == 0:
          return render_template('notfound.html')
+      else:
+         url = []
+         for book in book_names:
+            myquery = {'book_name':book}
+            mydoc = mycol.find(myquery)
+            for x in mydoc:
+               url.append(x['book_url'])
+         return redirect(url_for('book_url',bookurl = url))
+      # #获取对应url值
+      # myquery = {'book_name':{ "$regex": "^{}".format(book_name)}}
+      # mydoc = mycol.find(myquery)
+      # url = []
+      # for x in mydoc:
+      #    url.append(x['book_url'])
+      # if url:
+      #    return redirect(url_for('book_url',bookurl = url))
+      # else:
+      #    return render_template('notfound.html')
 
 
    elif request.method == 'GET':
